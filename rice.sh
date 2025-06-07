729f6a4548362cd1a12347b297948696ef89b734
Zophia Horvath <lestusj@gmail.com>
2025-06-07 12:24:33 -0400

    We tryin it this way

--- rice.sh
+++ rice.sh
@@ -22,16 +22,19 @@ NVIMP=$HOME/.config/nvim
 if [ ! -d "$HOME/neovim" ]; then
 	echo "[RICE] Installing: Neovim"
 	echo "[RICE] Echo disabled, please wait for message"
-
-	git clone https://github.com/neovim/neovim.git &> /dev/null
-	cd neovim
-	make CMAKE_BUILD_TYPE=Release
-	sudo make install
-	#- Plug installation -
+	wdir=$PWD # save where we're at
+	git clone https://github.com/neovim/neovim.git /tmp/nvim &> /dev/null
+	cd /tmp/nvim # Move to directory
+	make CMAKE_BUILD_TYPE=Release #configure installation
+	sudo make install &> /dev/null #install this girlie 
+	cd $wdir #leave neovim dir
+#- Plug installation -
 #	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
  #      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	cp $PWD/init.vim $NVIMP/init.vim
-	cp $PWD/lua $NVIMP/lua -r
+	#cp $PWD/init.vim $NVIMP/init.vim
+	#cp $PWD/lua $NVIMP/lua -r
+	cd neovim
+	cp * $NVIMP -r
 #	/usr/local/bin/nvim -c 'PlugInstall'
 	echo "[RICE] Installed: Neovim"
 else 
